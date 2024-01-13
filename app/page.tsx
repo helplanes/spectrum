38989b412ce8a19a3cdaa37282fc4315a4de6dde
KewKartik <108450560+kewkartik@users.noreply.github.com>
2024-01-13 10:13:31 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import Hero from "@/components/hero";
 import Cards from "@/components/cards";
@@ -7,6 +7,11 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+      <hr/>
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl p-8">
+        Events <Image className="mx-auto pb-8 display: inline" width="70" height="70" alt="image" src="https://i.imgur.com/zOrZlKk.png" />
+      </h1>
+      <hr/>
       <Cards  />
       <div className="bg-white flex flex-col items-center justify-center w-screen h-screen text-gray-600 ">
         Build in Progress
