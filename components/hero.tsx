506638cb2bf2428c873f47feea588be33d4f6f0f
KewKartik <108450560+kewkartik@users.noreply.github.com>
2024-01-14 14:06:01 +0530

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,31 +1,18 @@
-import Link from "next/link";
+
 import React from "react";
 import "@/components/ui/rays.css"
 
 
-const navigation = [
-  { name: "Events", href: "/" },
-  { name: "Team", href: "/" },
-];
-
 export default function hero() {
   return (
     <main className="overflow-hidden">
-      <div className="flex flex-col h-[100vh] pb-36 items-center justify-center bg-white dark:bg-black transition-bg">
+      <div className="flex flex-col h-[100vh] pb-36 items-center justify-center bg-black transition-bg">
         <div className="absolute inset-0 overflow-hidden">
           <div className="jumbo absolute -inset-[10px] opacity-50">
             <div className="flex flex-col items-center justify-center w-screen pt-32 pb-96 overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
               <nav className="my-8 animate-fade-in">
                 <ul className="flex items-center justify-center gap-4">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.href}
-                      href={item.href}
-                      className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
+     
                 </ul>
               </nav>
             </div>
