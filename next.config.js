a24a3b0f8e68d1d25a14243d19051183326caaea
kewkartik <108450560+kewkartik@users.noreply.github.com>
2024-09-30 14:51:20 +0530

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     
     images: {
-      domains: ['i.imgur.com','images.unsplash.com','leerob.io', 'fe.pccoepune.com', 'drsandeeppatil.weebly.com', 'horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app', 'instagram.fpnq7-2.fna.fbcdn.net', 'spectrumpccoe.github.io'],
+      domains: ['i.imgur.com','res.cloudinary.com','images.unsplash.com','leerob.io', 'fe.pccoepune.com', 'drsandeeppatil.weebly.com', 'horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app', 'instagram.fpnq7-2.fna.fbcdn.net', 'spectrumpccoe.github.io'],
     },
   }
 
